bfe5c7a4f519a3fd3cd7bf83b092358eee07bf65
ajlopez <webmaster@ajlopez.com>
2014-03-03 11:55:14 -0300

    additional tests

--- Tdd/geodistance/test_distancias.py
+++ Tdd/geodistance/test_distancias.py
@@ -32,6 +32,18 @@ class DistanciasTests(unittest.TestCase):
         self.assertTrue(result.longitud <= -2)
         self.assertTrue(result.longitud >= -2.1)
 
+    def test_distancia_112_kms_este_a_latitud_60_sur(self):
+        result = desplaza(-60, 0, 0, 112)
+        self.assertAlmostEqual(-60, result.latitud)
+        self.assertTrue(result.longitud >= 2)
+        self.assertTrue(result.longitud <= 2.1)
+
+    def test_distancia_112_kms_oeste_a_latitud_60_sur(self):
+        result = desplaza(-60, 0, 180, 112)
+        self.assertAlmostEqual(-60, result.latitud)
+        self.assertTrue(result.longitud <= -2)
+        self.assertTrue(result.longitud >= -2.1)
+
     def test_distancia_112_kms_norte(self):
         result = desplaza(0, 0, 90, 112)
         self.assertTrue(result.latitud >= 1)
@@ -43,6 +55,18 @@ class DistanciasTests(unittest.TestCase):
         self.assertTrue(result.latitud <= -1)
         self.assertTrue(result.latitud >= -1.1)
         self.assertAlmostEqual(0, result.longitud)
-                
+
+    def test_distancia_112_kms_norte_desde_punto_10(self):
+        result = desplaza(10, 10, 90, 112)
+        self.assertTrue(result.latitud >= 11)
+        self.assertTrue(result.latitud <= 11.1)
+        self.assertAlmostEqual(10, result.longitud)
+
+    def test_distancia_112_kms_sur_desde_punto_10(self):
+        result = desplaza(10, 10, 270, 112)
+        self.assertTrue(result.latitud <= 9)
+        self.assertTrue(result.latitud >= 8.9)
+        self.assertAlmostEqual(10, result.longitud)
+        
 if __name__ == '__main__':
     unittest.main()        
