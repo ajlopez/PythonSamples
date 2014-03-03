0e834d021c6ebd21fc76410a958827cc80e12dcc
ajlopez <webmaster@ajlopez.com>
2014-03-03 11:21:43 -0300

    east/west displacement

--- Tdd/geodistance/distancias.py
+++ Tdd/geodistance/distancias.py
@@ -1,9 +1,14 @@
 
+import math
+
 class DynamicObject(object):
     pass
+    
+TierraRadio = 6371 # kilometros
+TierraCircunferencia = TierraRadio * 2 * math.pi;
 
 def desplaza(lat, lon, angulo, distancia):
     punto = DynamicObject()
     punto.latitud = lat
-    punto.longitud = lon
+    punto.longitud = lon + math.cos(math.radians(angulo)) * distancia / (TierraCircunferencia / 360)
     return punto
--- Tdd/geodistance/test_distancias.py
+++ Tdd/geodistance/test_distancias.py
@@ -8,5 +8,15 @@ class DistanciasTests(unittest.TestCase):
         self.assertEqual(0, result.latitud)
         self.assertEqual(0, result.longitud)
 
+    def test_distancia_112_kms_este(self):
+        result = desplaza(0, 0, 0, 112)
+        self.assertEqual(0, result.latitud)
+        self.assertTrue(result.longitud >= 1)
+
+    def test_distancia_112_kms_oeste(self):
+        result = desplaza(0, 0, 180, 112)
+        self.assertEqual(0, result.latitud)
+        self.assertTrue(result.longitud <= -1)
+        
 if __name__ == '__main__':
     unittest.main()        
